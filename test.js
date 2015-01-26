a87524b6ab9e4fbfd9bcb692c664cf9f6dcde9f4
Ben Ng <me@benng.me>
2015-01-26 12:20:51 -0600

    Fix tests for new method names

--- test.js
+++ test.js
@@ -6,13 +6,13 @@ test('tz2tz', function (t) {
 
   t.equal(z(new Date(1421330745258), 'America/Los_Angeles', 'ddd MMM D'), 'Thu Jan 15')
   t.equal(z(1421330745258, 'America/Los_Angeles', 'ddd MMM D'), 'Thu Jan 15')
-  t.equal(z.d(1421330745258, 'America/Los_Angeles'), 'Thu Jan 15')
-  t.equal(z.t(1421330745258, 'America/Los_Angeles'), '6:05AM')
-  t.equal(z.f(1421330745258, 'America/Los_Angeles'), 'Thu, Jan 15, 2015 6:05 AM')
-  t.equal(z.s(1421330745258, 'America/Los_Angeles'), '6:05AM 1/15/15')
+  t.equal(z.getDate(1421330745258, 'America/Los_Angeles'), 'Thu Jan 15')
+  t.equal(z.getTime(1421330745258, 'America/Los_Angeles'), '6:05AM')
+  t.equal(z.getFull(1421330745258, 'America/Los_Angeles'), 'Thu, Jan 15, 2015 6:05 AM')
+  t.equal(z.getShort(1421330745258, 'America/Los_Angeles'), '6:05AM 1/15/15')
 
-  t.equal(z.day(1421330745258, 'America/Los_Angeles'), 'Thursday')
+  t.equal(z.getDay(1421330745258, 'America/Los_Angeles'), 'Thursday')
 
   z.constants.timeFormat = 'h:mm a'
-  t.equal(z.t(1421330745258, 'America/Los_Angeles'), '6:05 am')
+  t.equal(z.getTime(1421330745258, 'America/Los_Angeles'), '6:05 am')
 })
